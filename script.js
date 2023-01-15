2801422ad462f147c28b77cfe281823a7ed1926c
glmsr12 <gulumsercakmak@hotmail.com>
2023-01-15 01:39:48 -0800

    "ai response implemented"

--- script.js
+++ script.js
@@ -45,5 +45,51 @@ function generateUniqueId() {
   const randomNumber = Math.random();
   const hexadecimalString = randomNumber.toString(16);
 
-  return `id-${timestamp}-${hexadecimalString}`;
+  return `id-${timeStamp}-${hexadecimalString}`;
 }
+
+//chat stripe
+
+function chatStripe(isAi, value, uniqueId) {
+  return `
+    <div class="wrapper" ${isAi && 'ai'}>
+    <div class="chat">
+    <div class="profile">
+    <img src="${isAi ? bot : user}"
+     alt="${isAi ? 'bot' : 'user'}
+     />
+    </div>
+    <div class="message" id=${uniqueId}>${value}</div>
+    </div>
+     </div>
+    `;
+}
+
+//ai response
+
+const handleSubmit = async (e) => {
+  e.preventDefault();
+
+  const data = new FormData(form);
+
+  //chat stripe for user
+  chatContainer.innerHTML += chatStripe(false, data.get('prompt'));
+  form.reset();
+
+  //chat stripe for AI
+  const uniqueId = generateUniqueId();
+  chatContainer.innerHTML += chatStripe(true, '', uniqueId);
+
+  chatContainer.scrollTop = chatContainer.scrollHeight;
+
+  const messageDiv = document.getElementById(uniqueId);
+
+  loader(messageDiv);
+};
+
+form.addEventListener('submit', handleSubmit);
+form.addEventListener('keyup', (e) => {
+  if (e.keyCode === 13) {
+    handleSubmit(e);
+  }
+});
