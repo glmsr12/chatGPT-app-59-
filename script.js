1defbf397d6f878b6e723d534a0989e00a8042a1
glmsr12 <gulumsercakmak@hotmail.com>
2023-01-15 01:15:29 -0800

    "implement typing function"

--- script.js
+++ script.js
@@ -6,16 +6,44 @@ const chatContainer = document.querySelector('#chat_container');
 
 let loadInterval;
 
-//chatio loading
+//chatAI loading
 
 function loader(e) {
   e.textContent = '';
 
   loadInterval = setInterval(() => {
     e.textContent += '.';
-
+    //if loading indicator reaches three dots reset it
     if (e.textContent === '....') {
       e.textContent = '';
     }
   }, 300);
+}
+
+//typing function
+
+function typeText(e, text) {
+  let index = 0;
+
+  let interval = setInterval(() => {
+    //if we are still typing
+
+    if (index < text.length) {
+      //AI will check the character and will return the specific index
+      e.innerHTML += text.chartAt(index);
+      index++;
+    } else {
+      clearInterval(interval);
+    }
+  }, 20);
+}
+
+//generate unique id of each messages
+
+function generateUniqueId() {
+  const timeStamp = Date.now();
+  const randomNumber = Math.random();
+  const hexadecimalString = randomNumber.toString(16);
+
+  return `id-${timestamp}-${hexadecimalString}`;
 }
